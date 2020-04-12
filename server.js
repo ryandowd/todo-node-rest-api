98e4f1068f5c471c79ac5a412d12fb0b294388c0
Ryan Dowd <ryan.dowd84@gmail.com>
2020-04-12 22:44:25 +0100

    Adding the PUT request

--- server.js
+++ server.js
@@ -3,7 +3,23 @@ const bodyParser = require('body-parser');
 const _ = require('underscore');
 const app = express();
 const PORT = process.env.PORT || 3000;
-let todos = [];
+let todos = [
+  {
+      "completed": false,
+      "description": "111",
+      "id": 1
+  },
+  {
+      "completed": false,
+      "description": "1232311",
+      "id": 2
+  },
+  {
+      "completed": false,
+      "description": "1244432311",
+      "id": 3
+  }
+];
 let todoNextId = 1;
 
 app.use(bodyParser.json());
@@ -34,12 +50,11 @@ app.get('/todos/:id', (req, res) => {
 // POST /todos
 app.post('/todos', (req, res) => {
   const body = _.pick(req.body, 'completed', 'description');
-
-  const hasCompleted = _.isBoolean(body.completed);
+  const completedIsBool = _.isBoolean(body.completed);
   const hasDescription = _.isString(body.description);
   const blankDescription = body.description.trim().length === 0;
 
-  if (!hasCompleted || !hasDescription || blankDescription) {
+  if (!completedIsBool || !hasDescription || blankDescription) {
     return res.status(400).send();
   }
 
@@ -64,6 +79,46 @@ app.delete('/todos/:id', (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log('Express listening on PORT: ' + PORT);
-})
+// PUT /todos/:id
+app.put('/todos/:id', (req, res) => {
+  const body = _.pick(req.body, 'completed', 'description');
+  const validAttrs = {};
+  const hasCompleted = body.hasOwnProperty('completed');
+  const completedIsBool = _.isBoolean(body.completed);
+  const todoId = parseInt(req.params.id);
+  const matchedTodo = _.findWhere(todos, {id: todoId});
+
+  if (!matchedTodo) {
+    return res.status(404).json({
+      Error: "No matching ID found. You cannot update a TODO that does not exist"
+    });
+  }
+  
+  // 'Field: Completed'
+  if (hasCompleted && completedIsBool) {
+    validAttrs.completed = body.completed;
+  } else if (hasCompleted && !completedIsBool) {
+    return res.status(400).json({
+      Error: "'Completed' field needs to be a boolean"
+    });
+  }
+
+  const hasDescription = body.hasOwnProperty('description');
+  const descriptionIsString = _.isString(body.description);
+  const blankDescription = hasDescription && body.description.trim().length === 0;
+
+  // 'Field: Description'
+  if (hasDescription && descriptionIsString && !blankDescription) {
+    validAttrs.description = body.description.trim();
+  } else if (blankDescription) {
+    return res.status(400).json({
+      Error: "'Description' field cannot be blank"
+    });
+  }
+
+  _.extend(matchedTodo, validAttrs);
+  res.json(matchedTodo);
+
+});
+
+app.listen(PORT, () => {});
