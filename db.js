3fcffd529ae5b42e00fc1a7833c6499df7bc6345
Ryan Dowd <ryan.dowd84@gmail.com>
2020-04-14 14:17:29 +0100

    Add postgres for PROD

--- db.js
+++ db.js
@@ -1,8 +1,17 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(undefined, undefined, undefined, {
-    'dialect' : 'sqlite',
-    'storage' : __dirname + '/data/dev-todo-api.sqlite'
-});
+const env = process.env.NODE_ENV || 'development';
+let sequelize;
+
+if (env === 'production') {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres'
+    });
+} else {
+    sequelize = new Sequelize(undefined, undefined, undefined, {
+        'dialect': 'sqlite',
+        'storage': __dirname + '/data/dev-todo-api.sqlite'
+    });
+}
 
 let db = {};
 
